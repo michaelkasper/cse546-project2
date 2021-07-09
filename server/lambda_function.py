c71cb1d1de9883e9d98168836b62b3c1f0187123
Michael Kasper <mkasper1@godaddy.com>
2021-07-09 10:10:49 -0700

    added some more validation logic

--- server/lambda_function.py
+++ server/lambda_function.py
@@ -44,6 +44,21 @@ def convert_to_png(image):
     return buf.getvalue()
 
 
+def is_valid_request(qr_text, qr_position, img_dimensions, qr_dimensions):
+    if (
+            not qr_text or
+            qr_position[0] < 0 or
+            qr_position[1] < 0 or
+            img_dimensions[0] < 1 or
+            img_dimensions[1] < 1 or
+            qr_position[0] + qr_dimensions[0] > img_dimensions[0] or
+            qr_position[1] + qr_dimensions[1] > img_dimensions[1]
+    ):
+        return False
+
+    return True
+
+
 def lambda_handler(event, context):
     try:
         payload = decode_payload(event)
@@ -53,14 +68,39 @@ def lambda_handler(event, context):
         qr_position = (int(float(payload['qrPositionX'])), int(float(payload['qrPositionY'])))
         img_dimensions = (int(float(payload['imgSizeW'])), int(float(payload['imgSizeH'])))
         qr_dimensions = (int(float(payload['qrSizeW'])), int(float(payload['qrSizeH'])))
-        #error check on dimensions
-        if (qr_position[0] + qr_dimensions[0] > img_dimensions[0] or qr_position[1] + qr_dimensions[1] > img_dimensions[1]):
+
+        # error check
+        if not qr_text:
             return {
-                'statusCode': 400
+                'statusCode': 400,
+                'body': 'missing url'
             }
 
-            
-        image = Image.open(BytesIO(image_data))
+        if qr_position[0] < 0 or qr_position[1] < 0:
+            return {
+                'statusCode': 400,
+                'body': 'bad qr position'
+            }
+
+        if (
+                img_dimensions[0] < 1 or
+                img_dimensions[1] < 1 or
+                qr_position[0] + qr_dimensions[0] > img_dimensions[0] or
+                qr_position[1] + qr_dimensions[1] > img_dimensions[1]
+        ):
+            return {
+                'statusCode': 400,
+                'body': 'image too small'
+            }
+
+        try:
+            image = Image.open(BytesIO(image_data))
+        except (IOError, SyntaxError) as e:
+            return {
+                'statusCode': 400,
+                'body': 'bad image'
+            }
+
         image_resize = image.resize(img_dimensions)
 
         qr_img = create_qr_code(qr_text, qr_dimensions)
@@ -78,12 +118,16 @@ def lambda_handler(event, context):
 
     except ValueError as e:
         logger.info(e)
+        return {
+            'statusCode': 400,
+            'body': e
+        }
     except:
         logger.info(sys.exc_info()[0])
-
-    return {
-        'statusCode': 400
-    }
+        return {
+            'statusCode': 500,
+            'body': sys.exc_info()[0]
+        }
 
 # for local testing
 #
